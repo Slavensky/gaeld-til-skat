9cdbe0369aecd384d7c0f16f45834c7e2045d648
Slavensky <72447909+Slavensky@users.noreply.github.com>
2023-04-05 11:32:10 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -2,5 +2,5 @@ const test = () => {
 
     var randomNumber = Math.floor(Math.random() * 1000000);
 
-    document.getElementById('gaeld').innerHTML = `Du skylder ${randomNumber} kr. til skat.`;
+    document.getElementById('gaeld').innerHTML = `Du skylder <b>${randomNumber}</b> kr. til skat.`;
 }
